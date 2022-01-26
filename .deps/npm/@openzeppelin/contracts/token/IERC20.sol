77e3f8bc45092af4108f87820c17cfadba3dac5c
Prosper <sirpros@gmail.com>
2022-01-26 11:47:10 +0100

    Added smart contract

---
+++ .deps/npm/@openzeppelin/contracts/token/IERC20.sol
@@ -0,0 +1,32 @@
+FILE EXPLORERS
+Zoom outZoom in
+Scroll to see all tabs
+12345678910111213141516171819202122232425262728293031323334353637383940
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+
+Clear console
+0
+listen on network
+Search with transaction hash or address
+ - Welcome to Remix 0.20.3 - 
+
+You can use this terminal to: 
+Check transactions details and start debugging.
+Execute JavaScript scripts:
+ - Input a script directly in the command line interface 
+ - Select a Javascript file in the file explorer and then run \`remix.execute()\` or \`remix.exeCurrent()\`  in the command line interface  
+ - Right click on a JavaScript file in the file explorer and then click \`Run\` 
+The following libraries are accessible:
+web3 version 1.5.2
+ethers.js 
+remix (run remix.help() for more info)
+>
